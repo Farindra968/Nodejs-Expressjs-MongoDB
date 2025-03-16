740000ef1189743082baf1bdb5056af30926f6e0
Farindra968 <farindrabahadurbhandari@gmail.com>
2025-03-16 19:41:39 +0545

    feat: add error handling in login controller and improve cookie management

--- src/controller/authController.js
+++ src/controller/authController.js
@@ -1,9 +1,10 @@
 import authServices from "../service/authServices.js"
 
 const login = async (req, res) => {
+    /// try catch to handel errors
     try {
         const login = await authServices.login(req.body)
-        res.cookie("userId", login._id);
+        res.cookie("userId", login._id); /// kepping user id in cookie
         res.json(login)
         
     } catch (error) {
