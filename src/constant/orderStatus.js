164815b2ccc735581e697515013371056a0b06a6
Farindra968 <farindrabahadurbhandari@gmail.com>
2025-04-02 23:29:43 +0545

    feat: add Order model schema with status management and order details

---
+++ src/constant/orderStatus.js
@@ -0,0 +1,4 @@
+export const PENDING_ORDER = "Pending"
+export const COMPLETE_ORDER = "Complete"
+export const SHIFT_ORDER = "Shift"
+export const DELiVERED_ORDER = "Delivered"
--- src/modules/order.js
+++ src/modules/order.js
@@ -1,8 +1,47 @@
 import mongoose, { Schema } from "mongoose";
+import {
+  COMPLETE_ORDER,
+  DELiVERED_ORDER,
+  PENDING_ORDER,
+  SHIFT_ORDER,
+} from "../constant/orderStatus.js";
 
 const OrderSchema = new mongoose.Schema({
-    
-})
+  orderNumder: {
+    type: String,
+  },
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: "user",
+  },
+  orderItems: [
+    {
+      product: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: "Product",
+      },
+      quantity: {
+        type: String,
+        default: 1,
+      },
+    },
+  ],
+  totaPrice: {
+    type: Number,
+    required: true,
+  },
+  OrderStatus: {
+    type: String,
+    default: PENDING_ORDER,
+    enum: [PENDING_ORDER, COMPLETE_ORDER, SHIFT_ORDER, DELiVERED_ORDER],
+  },
+  createdAt: {
+    type: String,
+    default: Date.now(),
+  },
+});
 
 const model = mongoose.model("Order", OrderSchema);
 
