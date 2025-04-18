d3024dda9daac5f672d68cb300dfe09852eafc39
Farindra968 <farindrabahadurbhandari@gmail.com>
2025-04-18 17:12:52 +0545

    fix: add missing newline for improved code readability in orderRoutes.js

--- src/routes/orderRoutes.js
+++ src/routes/orderRoutes.js
@@ -3,6 +3,7 @@ import { checkOutOrder, createOrder, deleteOrder, getAllOrders, getOrderByID, ge
 import {ADMIN_ROLE} from "../constant/role.js"
 import authMiddleware from "../middlewares/authMiddlewares.js"
 import roleBasedAuth from "../middlewares/roleBasedAuth.js";
+
 const router = express.Router();
 
 router.get("/", authMiddleware, roleBasedAuth(ADMIN_ROLE), getAllOrders);
